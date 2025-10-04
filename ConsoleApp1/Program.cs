c251692fae5284215d31a38b3764008a7e089575
harshalpande20 <harsh.pande35@gmail.com>
2025-10-04 12:52:28 +0530

    add new task for loop

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,13 @@
 ﻿
 
 
+using ConsoleApp1.loops;
+
 class Program
 {
     public static void Main(string[] args)
     {
         System.Console.WriteLine("Hello, World!");
+        LoopsTask.Task();
     }
 }
---
+++ ConsoleApp1/loops/LoopsTask.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.loops
+{
+    public class LoopsTask
+    {
+        public static void Task()
+        {
+            // Print numbers from 1 to 10 using a for loop
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
